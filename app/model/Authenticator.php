6efca51737b46b424d7e4e0b2cb8d3aab3a6da0f
Unknown <vojmel@gmail.com>
2017-12-03 20:15:46 +0100

    Auto stash before merge of "master" and "origin/master"
    Přihlašování

--- app/model/Authenticator.php
+++ app/model/Authenticator.php
@@ -4,6 +4,8 @@ namespace App\Model;
 
 use Nette;
 use Nette\Security;
+use Nette\Security\AuthenticationException;
+use Nette\Security\Identity;
 
 
 /**
@@ -22,25 +24,24 @@ class Authenticator extends Nette\Object implements Security\IAuthenticator
 
 
 	/**
-	 * Performs an authentication.
-	 * @return Nette\Security\Identity
-	 * @throws Nette\Security\AuthenticationException
+	 * @return Identity
+     * @throws Nette\Security\AuthenticationException
 	 */
 	public function authenticate(array $credentials)
 	{
 		list($username, $password) = $credentials;
-		$row = $this->database->table('users')->where('username', $username)->fetch();
+		$row = $this->database->table('uzivatel')->where('username', $username)->fetch();
 
 		if (!$row) {
-			throw new Security\AuthenticationException('The username is incorrect.', self::IDENTITY_NOT_FOUND);
-
-		} elseif (!Security\Passwords::verify($password, $row->password)) {
-			throw new Security\AuthenticationException('The password is incorrect.', self::INVALID_CREDENTIAL);
+			throw new Security\AuthenticationException('Špatné uživatelské jméno.', self::IDENTITY_NOT_FOUND);
 		}
+        if ($row->password !== sha1($password . UzivatelManager::$user_salt)) {
+            throw new AuthenticationException("Špatné uživatelské heslo.", self::INVALID_CREDENTIAL);
+        }
 
 		$arr = $row->toArray();
 		unset($arr['password']);
-		return new Security\Identity($row->id, NULL, $arr);
-	}
 
+		return new Identity($row->uzivatelID, $row->roleID, $arr);
+	}
 }
